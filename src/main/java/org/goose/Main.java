3bfe053e0eb1ea98403ea0cf290086fbf3b9aa64
GUS\Vladimir <atis-magnus@mail.ru>
2024-07-27 02:28:25 +0300

    job's done

--- src/main/java/org/goose/Main.java
+++ src/main/java/org/goose/Main.java
@@ -3,6 +3,14 @@ package org.goose;
 public class Main {
 
     public static void main(String[] args) {
+        //  2. Создать массив из 5 сотрудников.
+        Employee[] empArray = new Employee[5];
+        empArray[0] = new Employee("Ivanov Ivan", "Engineer", "ivivan@mailbox.com", "892312312", 30000, 30);
+        empArray[1] = new Employee("Maximov Max", "Designer", "maxmax@mailbox.com", "892312313", 150000, 38);
+        empArray[2] = new Employee("Gusev Gus", "Programmer", "gugus@mailbox.com", "892312314", 29000, 42);
+        empArray[3] = new Employee("Krupskaya Nadezhda", "HR", "krunadezhda@mailbox.com", "892312315", 3000, 29);
+        empArray[4] = new Employee("Dedov Ded", "Security", "dded@mailbox.com", "892312316", 2000, 64);
 
+        empArray[3].printFullInfo();
     }
 }
--- src/main/java/org/goose/Park.java
+++ src/main/java/org/goose/Park.java
@@ -1,5 +1,48 @@
 package org.goose;
 
+//3. Создать класс Park с внутренним классом, с помощью объектов которого можно хранить информацию об аттракционах, времени их работы и стоимости.
 public class Park {
 
+    public class Attraction {
+        private String name;
+        private String openingHours;
+        private int ticketPrice;
+
+        public Attraction(String name, String openingHours, int ticketPrice) {
+            this.name = name;
+            this.openingHours = openingHours;
+            this.ticketPrice = ticketPrice;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getOpeningHours() {
+            return openingHours;
+        }
+
+        public void setOpeningHours(String openingHours) {
+            this.openingHours = openingHours;
+        }
+
+        public double getTicketPrice() {
+            return ticketPrice;
+        }
+
+        public void setTicketPrice(int ticketPrice) {
+            this.ticketPrice = ticketPrice;
+        }
+
+        public void printFullInfo() {
+            System.out.println("Название аттракциона: " + name + "\n"
+                    + "Время работы: " + openingHours + "\n"
+                    + "Стоимость билета: " + ticketPrice + "\n");
+        }
+    }
 }
+
