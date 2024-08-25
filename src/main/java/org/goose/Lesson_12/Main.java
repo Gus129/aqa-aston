2b629fdff9c04c5c5240d5db5fcd12e0bd14aff2
GUS\Vladimir <atis-magnus@mail.ru>
2024-08-25 14:56:14 +0300

    Refactored static method,new class MyArrayCalculation

--- src/main/java/org/goose/Lesson_12/Main.java
+++ src/main/java/org/goose/Lesson_12/Main.java
@@ -2,28 +2,6 @@ package org.goose.Lesson_12;
 
 public class Main {
 
-    public static void sumOfArrayElements(String[][] array) {
-        try {
-            if (array.length != 4 || array[0].length != 4) {
-                throw new MyArraySizeException("Размер массива должен быть строго 4x4");
-            }
-            int sum = 0;
-            for (int i = 0; i < array.length; i++) {
-                for (int j = 0; j < array[i].length; j++) {
-                    try {
-                        int element = Integer.parseInt(array[i][j]);
-                        sum += element;
-                    } catch (NumberFormatException e) {
-                        throw new MyArrayDataException("Неверные данные в массиве по координатам: (" + i + "," + j + "), значение: '" + array[i][j] + "'");
-                    }
-                }
-            }
-            System.out.println("Сумма элементов массива: " + sum);
-        } catch (MyArraySizeException | MyArrayDataException e) {
-            e.printStackTrace();
-        }
-    }
-
     public static void main(String[] args) {
         String[][] validArray = {
                 {"1", "2", "3", "4"},
@@ -46,8 +24,27 @@ public class Main {
                 {"девять", "10", "11", "12"},
                 {"13", "14", "15", "16"}
         };
-        sumOfArrayElements(wrongDataArray);
-        sumOfArrayElements(wrongSizeArray);
-        sumOfArrayElements(validArray);
+
+        MyArrayCalculation validCalc = new MyArrayCalculation(validArray);
+        MyArrayCalculation wrongSizeCalc = new MyArrayCalculation(wrongSizeArray);
+        MyArrayCalculation wrongDataCalc = new MyArrayCalculation(wrongDataArray);
+
+        try {
+            validCalc.sumOfArrayElements();
+        } catch (MyArraySizeException | MyArrayDataException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            wrongSizeCalc.sumOfArrayElements();
+        } catch (MyArraySizeException | MyArrayDataException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            wrongDataCalc.sumOfArrayElements();
+        } catch (MyArraySizeException | MyArrayDataException e) {
+            e.printStackTrace();
+        }
     }
 }
